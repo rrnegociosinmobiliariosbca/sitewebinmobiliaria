feb16e473870f1490012d4ccab030ef79a0ffe1b
maoxtrem <maoxtrem@gmail.com>
2025-05-27 20:30:02 -0500

    ajustes

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\PropertyDetalleRepository;
+use App\Repository\PropertyImageRepository;
 use App\Repository\PropertyRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -31,7 +33,19 @@ final class DashboardController extends AbstractController
         }
 
 
-        $property = new \App\Entity\Property();
+        // validar si id es mayor a 0 entonces es una actualización
+        if (isset($data['id']) && $data['id'] > 0) {
+            $property = $propertyRepository->find($data['id']);
+            if (!$property) {
+                return $this->json([
+                    'status' => 'error',
+                    'message' => 'Inmueble no encontrado',
+                ], Response::HTTP_NOT_FOUND);
+            }
+        } else {
+            // Si no se proporciona un ID, se crea un nuevo objeto Property
+            $property = new \App\Entity\Property();
+        }
         $property->setInmueble($data['inmueble']);
         $property->setValor($data['valor']);
         $property->setDireccion($data['direccion']);
@@ -89,4 +103,44 @@ final class DashboardController extends AbstractController
     }
 
 
+    #[Route('/dashboard/endpoint/eliminar_inmueble/{id}', name: 'app_dashboard_endpoint_eliminar_inmueble')]
+    public function app_dashboard_endpoint_eliminar_inmueble(
+        int $id,
+        PropertyRepository $propertyRepository,
+        PropertyDetalleRepository $propertyDetalleRepository,
+        PropertyImageRepository $propertyImageRepository
+    ): JsonResponse {
+        // Buscar el registro por ID
+        $property = $propertyRepository->find($id);
+
+        if (!$property) {
+            return $this->json([
+                'status' => 'error',
+                'message' => 'Inmueble no encontrado',
+            ], Response::HTTP_NOT_FOUND);
+        }
+
+        //eliminar detalles asociados
+        $propertyDetalles = $property->getPropertyDetalles();
+        foreach ($propertyDetalles as $detalle) {
+            $propertyDetalleRepository->remove($detalle);
+        }
+        //eliminar imagenes asociadas
+        $propertyImages = $property->getPropertyImages();
+        foreach ($propertyImages as $image) {
+            //eliminar imagenes con unlink
+            $propertyImageRepository->remove($image);
+            $imagePath = $this->getParameter('imagenes_directory') . '/' . basename($image->getUrl());
+            if (file_exists($imagePath)) {
+                unlink($imagePath);
+            }
+        }
+
+        $propertyRepository->remove($property);
+
+        return $this->json([
+            'status' => 'success',
+            'message' => 'Inmueble eliminado correctamente',
+        ]);
+    }
 }
